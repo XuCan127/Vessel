bbcaf4737c902fe629c4ccfa634d7e5b3e8609ef
payload <125278602+XuCan127@users.noreply.github.com>
2023-07-26 22:36:25 +0800

    Update cgroup.go

--- src/engine/module/cgroup/cgroup.go
+++ src/engine/module/cgroup/cgroup.go
@@ -1,1 +1,43 @@
 package cgroup
+
+import (
+	"github.com/Vessel/src/engine/module/cgroup/subsystem"
+	"path"
+)
+
+const (
+	CGroupPath = "/sys/fs/cgroup"
+)
+
+func SetCGroup(pid int, containerName string) error {
+
+	var (
+		cpuPath    = path.Join(CGroupPath, "cpu", containerName)
+		memoryPath = path.Join(CGroupPath, "memory", containerName)
+	)
+	// 设置cpu
+	if err := subsystem.SetCpuCGroup(cpuPath, pid); err != nil {
+		panic(err)
+	}
+	// 设置内存
+	if err := subsystem.SetMemoryCGroup(memoryPath, pid); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func CleanCGroup(containerName string) error {
+
+	var (
+		cpuPath    = path.Join(CGroupPath, "cpu", containerName)
+		memoryPath = path.Join(CGroupPath, "memory", containerName)
+	)
+	if err := subsystem.CleanCpuCGroup(cpuPath); err != nil {
+		panic(err)
+	}
+	if err := subsystem.CleanMemoryCGroup(memoryPath); err != nil {
+		panic(err)
+	}
+	return nil
+
+}
