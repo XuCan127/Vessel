c665b078155cded28bc784f7c572349098e590b9
payload <125278602+XuCan127@users.noreply.github.com>
2023-08-09 23:50:05 +0800

    Add files via upload

--- src/server/module/cgroup/subSystems/memory.go
+++ src/server/module/cgroup/subSystems/memory.go
@@ -9,11 +9,15 @@ import (
 	"syscall"
 )
 
-func SetMemoryCGroup(memoryPath string, pid int) error {
+const (
+	memoryLimit = "memory.limit_in_bytes"
+)
+
+func SetMemoryCGroup(memoryPath string, pid int, res *ResourceConfig) error {
 	if err := os.MkdirAll(memoryPath, 0700); err != nil {
 		return fmt.Errorf("create cgroup path fail err=%s", err)
 	}
-	if err := ioutil.WriteFile(path.Join(memoryPath, "memory.limit_in_bytes"), []byte("200m"), 0700); err != nil {
+	if err := ioutil.WriteFile(path.Join(memoryPath, memoryLimit), []byte(res.Memory), 0700); err != nil {
 		return fmt.Errorf("write memory limit bytes fail err=%s", err)
 	}
 	if err := ioutil.WriteFile(path.Join(memoryPath, "tasks"), []byte(strconv.Itoa(pid)), 0644); err != nil {
---
+++ src/server/module/cgroup/subSystems/subSystems.go
@@ -0,0 +1,7 @@
+package subSystems
+
+type ResourceConfig struct {
+	CpuSet   string
+	CpuShare string
+	Memory   string
+}
